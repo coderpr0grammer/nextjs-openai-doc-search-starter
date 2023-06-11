98ef20b9a6c5ee933f6c59214a1144b1d6452243
Daniel Martinez <daniellhf@icloud.com>
2023-06-11 10:54:22 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home() {
 
         {/* File upload overlay */}
       {isDragging && (
-        <div className={styles.overlay}>
+        <div className={styles.overlay} style={{background: 'red'}}>
           <div className={styles.uploadIcon}>
             <FontAwesomeIcon icon={faCloudArrowUp} color="white" />
           </div>
@@ -71,7 +71,7 @@ export default function Home() {
 
       {/* File preview */}
       {selectedFile && (
-        <div className={styles.preview}>
+        <div className={styles.preview} style={{background: 'red'}}>
           {/* Render file preview here */}
         </div>
       )}
