3dbe1d67b72e618bf7e1e95bfd6a5f5a6c4e1f33
Daniel Martinez <daniellhf@icloud.com>
2023-06-10 22:15:59 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    apiTimeout: 10000, // Set the timeout to 10 seconds
+  },
 }
 
 module.exports = nextConfig
